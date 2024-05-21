d0090959c4d2a7dd72c6a5a434415aa811422cb7
Ayush Goyal <ayush.goyal@winzogames.com>
2024-05-21 16:46:29 +0530

    project-completed

---
+++ Football Chants/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football Chants
+//
+//  Created by Ayush Goyal on 14/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football Chants/Football_ChantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_ChantsApp.swift
+//  Football Chants
+//
+//  Created by Ayush Goyal on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Football_ChantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
